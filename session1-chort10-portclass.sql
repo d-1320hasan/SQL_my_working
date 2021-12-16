805b61c00022524188331e5db2743afa6980baee
d-1320hasan <hasantaskomur@gmail.com>
2021-12-16 19:43:39 +0300

    session-3

--- session1-chort10-portclass.sql
+++ session1-chort10-portclass.sql
@@ -41,3 +41,41 @@
 
 	SELECT *
 	FROM invoices
+	---SESSİON -2
+	SELECT * 
+	FROM invoices 
+	WHERE total>10 LIMIT 2;
+	--LIMIT İLE KAÇ TANESİNİ GÖSTERECEĞİNİ SINIRLARIZ 
+	SELECT * 
+	FROM invoices 
+	WHERE total>10 LIMIT 4;
+	
+	--LIMIT İLE KAÇ TANESİNİ GÖSTERECEĞİNİ SINIRLARIZ
+	SELECT InvoiceId, InvoiceDate, total
+	FROM invoices 
+	WHERE total>10
+	LIMIT 4;
+	
+	/*===================================================
+													ORDER BY
+====================================================*/
+	
+	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtları Total değerine göre 
+	ARTAN sırada sıralayarak sorgulayiniz */
+	
+	SELECT *
+	FROM invoices
+	WHERE total>10
+	ORDER BY total ASC    --ASC YAZMASAYDIKDA ASC KABUL EDERDİ.
+	LIMIT 10 ;
+
+	/*invoices tablosunda Total değeri 10$'dan büyük olan kayıtlardan işlem tarihi 
+	(InvoiceDate) 	en yeni olan 10 kaydın tüm bilgilerini listeyiniz */ 
+	
+	SELECT *
+	FROM invoices
+	WHERE total>10
+	ORDER BY InvoiceDate DESC  
+	LIMIT 10;  
+	
+	
